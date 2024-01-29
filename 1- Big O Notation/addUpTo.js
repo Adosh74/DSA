6ad84ac1d6b168850aee30cdf644f5c85410246f
Mohamed Hamdy <98131964+Adosh74@users.noreply.github.com>
2024-01-29 05:19:19 +0200

    + code for "addUpTo" problem

---
+++ 1- Big O Notation/addUpTo.js
@@ -0,0 +1,17 @@
+// *** first think
+// const addUpTo = (n) => {
+//     let total = 0;
+//     for(let i = 1 ; i <= n ; i++){
+//         total += i;
+//     }
+//     return total;
+// }
+
+// *** same function using mathematical formula
+
+const addUpTo = (n) => {
+    return n * (n + 1) / 2;
+}
+const start = Date.now();
+console.log(addUpTo(1000000000));
+console.log('time take', Date.now() - start, 'ms');
